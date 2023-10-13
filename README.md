ab62be18f1efccc8377027e0bf0cba1da3505133
Anonoei <dev@anonoei.com>
2023-10-12 21:08:44 -1000

    Add calculating accel/velocity
    Added more specific config/macro min/max values
    Removed more un-needed variables
    Added TEST_ATTEMPTS, to test again if it fails
    Added 1 to accel/step count so it maxes at your _STOP
    Cleaned up console output
    Added times to pattern to show time delta for different accel/velocities
    Added _calc_velocity to calculate maximum velocity given accel
    Added _calc_accel to calculate maximum accel given velocity
    Added _calc_travel to calculate maximum travel distance
    Re-implemented hard-coded _START values

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 
 This module automatically performs [Ellis' TEST_SPEED macro](https://ellis3dp.com/Print-Tuning-Guide/articles/determining_max_speeds_accels.html), and measured the missed steps on your steppers at various accelerations/velocities. Depending on your ACCEL_START and VELOCITY_START, this may take a long time to finish.
 
+**Sensorless homing**: If you're using sensorless homing you may find that `MAX_MISSED=1.0` is too low. the endstop variance check will tell you how many steps you lose when homing. For instance, on my printer I lose around 0-4.2 steps each home. I run `AUTO_SPEED MAX_MISSED=5.0` to account for that variance. If you have one wildly different result, the default `TEST_ATTEMPTS` of 2 should catch it and keep going.
+
 # Table of Contents
  - [Overview](https://github.com/Anonoei/klipper_auto_speed#overview)
  - [How does it work](https://github.com/Anonoei/klipper_auto_speed#how-does-it-work)
@@ -24,9 +26,23 @@ This module automatically performs [Ellis' TEST_SPEED macro](https://ellis3dp.co
     1. Check endstop variance
        - Validate the endstops are accurate enough for `MAX_MISSED`
     2. Find the maximum acceleration
-       - Given `ACCEL_START`, `ACCEL_STOP_` `ACCEL_STEP`, increase acceleration until we miss more than `MAX_MISSED` steps. Move the toolhead over the pattern at each velocity `TEST_ITERATION` times
+       - accel = `ACCEL_START`
+       - velocity = `(math.sqrt(travel/accel)*accel) / math.log10(accel)`
+       1. Home, and save stepper start steps
+       2. Move over the pattern at `accel`/`velocity` `TEST_ITERATION` times
+       3. Home, and save stepper stop steps
+       4. Check if between start and stop steps we missed more than `MAX_MISSED` steps.
+          1. If we missed more than `MAX_MISSED`, go to the next step
+          2. Otherwise, accel += `ACCEL_STEP`
     3. Find maximum velocity
-       - Given `VELOCITY_START`, `VELOCITY_STOP_` `VELOCITY_STEP`, increase velocity until we miss more than `MAX_MISSED` steps. Move the toolhead over the pattern at each velocity `TEST_ITERATION` times
+       - velocity = `VELOCITY_START`
+       - accel = `(velocity**2/travel) * 4`
+       1. Home, and save stepper start steps
+       2. Move over the pattern at `accel`/`velocity` `TEST_ITERATION` times
+       3. Home, and save stepper stop steps
+       4. Check if between start and stop steps we missed more than `MAX_MISSED` steps.
+          1. If we missed more than `MAX_MISSED`, go to the next step
+          2. Otherwise, velocity += `VELOCITY_STEP`
     4. Stress test
        - Start at max acceleration/velocity and slowly decrease both until less than `MAX_MISSED` steps are lost after `STRESS_INTERATION`s.
 
@@ -81,13 +97,14 @@ max_missed: 1.0       ; Maximum full steps that can be missed
 endstop_samples: 3    ; How many endstop samples to take for endstop variance
 
 test_iterations: 2    ; While testing for maximum, perform the test movement this many times
+test_attempts: 2      ; Re-test this many times if test fails
 stress_iterations: 50 ; While finding final maximums, perform the test movement this many times
 
-accel_start: Unset    ; Starting acceleration, Defaults to your printer's current acceleration
+accel_start: 1000.0   ; Starting acceleration
 accel_stop: 50000.0   ; Maximum possible acceleration the test will go to
 accel_step: 1000.0    ; Increase accel_start by this amount each test
 
-velocity_start: Unset ; Starting velocity, Defaults to your printer's current velocity
+velocity_start: 100.0 ; Starting velocity
 velocity_stop: 5000.0 ; Maximum possible velocity the test will go to
 velocity_step: 50.0   ; Increase velocity_start by this amount each test
 ```
@@ -103,10 +120,11 @@ SETTLING_HOME     | 1       | Perform settling home before starting Auto Speed
 MAX_MISSED        | 1.0     | Maximum full steps that can be missed
 ENDSTOP_SAMPLES   | 3       | How many endstop samples to take for endstop variance
 TEST_ITERATIONS   | 2       | While testing for maximum, perform the test movement this many times
+TEST_ATTEMPTS     | 2       | Re-test this many times if test fails
 STRESS_ITERATIONS | 50      | While finding final maximums, perform the test movement this many times
-ACCEL_START       | Unset   | Starting acceleration, Defaults to your printer's current acceleration
+ACCEL_START       | 1000.0  | Starting acceleration
 ACCEL_STOP        | 50000.0 | Maximum possible acceleration the test will go to
 ACCEL_STEP        | 1000.0  | Increase accel_start by this amount each test
-VELOCITY_START    | Unset   | Starting velocity, Defaults to your printer's current velocity
+VELOCITY_START    | 100.0   | Starting velocity
 VELOCITY_STOP     | 5000.0  | Maximum possible velocity the test will go to
 VELOCITY_STEP     | 50.0    | Increase velocity_start by this amount each test
--- auto_speed.py
+++ auto_speed.py
@@ -3,6 +3,8 @@
 # Copyright (C) 2023 Anonoei <dev@anonoei.com>
 #
 # This file may be distributed under the terms of the MIT license.
+import math
+from time import perf_counter
 
 class AutoSpeed:
     def __init__(self, config):
@@ -10,23 +12,24 @@ class AutoSpeed:
         self.printer = config.get_printer()
 
         self.z = config.getfloat('z', default=50)
-        self.margin = config.getint('margin', default=20)
-        self.pattern_margin = config.getint('pattern_margin', default=20)
+        self.margin = config.getfloat('margin', default=20.0, above=0.0)
+        self.pattern_margin = config.getfloat('pattern_margin', default=20.0, above=0.0)
 
         self.settling_home = config.getboolean('settling_home', default=True)
         self.max_missed = config.getfloat('max_missed', default=1.0)
-        self.endstop_samples = config.getint('endstop_samples', default=3)
+        self.endstop_samples = config.getint('endstop_samples', default=3, minval=2)
 
-        self.test_iterations = config.getint('test_iterations', default=2)
-        self.stress_iterations = config.getint('stress_iterations', default=50)
+        self.test_iterations = config.getint('test_iterations', default=2, minval=1)
+        self.test_attempts   = config.getint('test_attempts', default=2, minval=0)
+        self.stress_iterations = config.getint('stress_iterations', default=50, minval=1)
 
-        self.accel_start = config.getfloat('accel_start', default=None)
-        self.accel_stop = config.getfloat('accel_stop', default=50000.0)
-        self.accel_step = config.getfloat('accel_step', default=1000.0)
+        self.accel_start = config.getfloat('accel_start', default=1000.0, above=0.0)
+        self.accel_stop = config.getfloat('accel_stop', default=50000.0, above=self.accel_start)
+        self.accel_step = config.getfloat('accel_step', default=1000.0, above=0.0, below=self.accel_stop)
 
-        self.veloc_start = config.getfloat('velocity_start', default=None)
-        self.veloc_stop = config.getfloat('velocity_stop',   default=5000.0)
-        self.veloc_step = config.getfloat('velocity_step',   default=50.0)
+        self.veloc_start = config.getfloat('velocity_start', default=100.0, above=0.0)
+        self.veloc_stop = config.getfloat('velocity_stop',   default=5000.0, above=self.veloc_start)
+        self.veloc_step = config.getfloat('velocity_step',   default=50.0, above=0.0, below=self.veloc_stop)
 
         self.toolhead = None
         self.printer.register_event_handler("klippy:connect", self.handle_connect)
@@ -37,10 +40,7 @@ class AutoSpeed:
                                     self.cmd_AUTO_SPEED,
                                     desc=self.cmd_AUTO_SPEED_help)
         
-        self.th_veloc = None
-        self.th_accel = None
         self.level = None
-        self.level_applied = False
         
         self.steppers = {}
         self.axes = {}
@@ -50,11 +50,6 @@ class AutoSpeed:
         self.th_accel = self.toolhead.max_accel
         self.th_veloc = self.toolhead.max_velocity
 
-        if self.accel_start is None:
-            self.accel_start = self.th_accel
-        if self.veloc_start is None:
-            self.veloc_start = self.th_veloc
-
         # Find and define leveling method
         if self.printer.lookup_object("quad_gantry_level", None) is not None:
             self.level = "QGL"
@@ -62,7 +57,6 @@ class AutoSpeed:
             self.level = "STA"
         else:
             self.level = None
-            self.level_applied = True
 
     def handle_home_rails_end(self, homing_state, rails):
         # Get rail min and max values
@@ -96,26 +90,27 @@ class AutoSpeed:
         if not len(self.steppers.keys()) == 3:
             raise gcmd.error(f"Printer must be homed first! Found {len(self.steppers.keys())} homed axes.")
         z = gcmd.get_float("Z", self.z)
-        margin = gcmd.get_float("MARGIN", self.margin)
-        pattern_margin = gcmd.get_float('PATTERN_MARGIN', self.pattern_margin)
+        margin = gcmd.get_float("MARGIN", self.margin, above=0.0)
+        pattern_margin = gcmd.get_float('PATTERN_MARGIN', self.pattern_margin, above=0.0)
 
         settling_home = gcmd.get_int("SETTLING_HOME", default=self.settling_home, minval=0, maxval=1)
         max_missed = gcmd.get_float('MAX_MISSED', self.max_missed, above=0.0)
         endstop_samples = gcmd.get_int('ENDSTOP_SAMPLES', self.endstop_samples, minval=2)
 
-        test_iterations = gcmd.get_int("TEST_ITERATIONS", self.test_iterations)
-        stress_iterations = gcmd.get_int("STRESS_ITERATIONS", self.stress_iterations)
+        test_iterations = gcmd.get_int("TEST_ITERATIONS", self.test_iterations, minval=1)
+        test_attempts = gcmd.get_int("TEST_ATTEMPTS", self.test_attempts, minval=0)
+        stress_iterations = gcmd.get_int("STRESS_ITERATIONS", self.stress_iterations, minval=1)
 
         accel_start = gcmd.get_float('ACCEL_START', self.accel_start, above=0.0)
         accel_stop = gcmd.get_float('ACCEL_STOP', self.accel_stop, above=accel_start)
-        accel_step = gcmd.get_float('ACCEL_STEP', self.accel_step, above=0.0)
+        accel_step = gcmd.get_float('ACCEL_STEP', self.accel_step, above=0.0, below=accel_stop)
 
         veloc_start = gcmd.get_float('VELOCITY_START', self.veloc_start, above=0.0)
         veloc_stop = gcmd.get_float('VELOCITY_STOP', self.veloc_stop, above=veloc_start)
-        veloc_step = gcmd.get_float('VELOCITY_STEP', self.veloc_step, above=0.0)
+        veloc_step = gcmd.get_float('VELOCITY_STEP', self.veloc_step, above=0.0, below=veloc_stop)
 
-
-        if self.level is not None and not self.level_applied:
+        # Level the printer if it's not leveled
+        if self.level is not None:
             lookup = None
             name = None
             if self.level == "QGL":
@@ -132,8 +127,7 @@ class AutoSpeed:
                 self.gcode._process_commands([name], False)
                 if level.z_status.applied is False:
                     raise gcmd.error(f"Failed to level printer! Please manually ensure your printer is level.")
-            self.level_applied = True
-        self._move([self.axes["x"]["center"], self.axes["y"]["center"], z], veloc_start)
+        self._move([self.axes["x"]["center"], self.axes["y"]["center"], z], self.th_veloc)
 
         if settling_home:
             self.toolhead.wait_moves()
@@ -177,8 +171,8 @@ class AutoSpeed:
             raise gcmd.error(f"Please increase MAX_MISSED (currently {max_missed}), or tune your steppers/homing macro.")
 
         # Perform tests
-        accel_count = int((accel_stop - accel_start) / accel_step)
-        veloc_count = int((veloc_stop - veloc_start) / veloc_step)
+        accel_count = int((accel_stop - accel_start) / accel_step) + 1
+        veloc_count = int((veloc_stop - veloc_start) / veloc_step) + 1
 
         positions = {
             "x": {
@@ -195,51 +189,81 @@ class AutoSpeed:
             }
         }
 
+        pos_travel = self._calc_travel(x=(positions["x"]["max"] - positions["x"]["min"]),
+                                       y=(positions["y"]["max"] - positions["y"]["min"]))
+
         # Find acceleration maximum
         measured_accel = None
+        measured_accel_veloc = None
+        attempt = 1
         for step in range(0, accel_count):
             accel = accel_start + (accel_step * step)
-            veloc = veloc_start
-            valid, missed_x, missed_y = self._test(veloc, accel, positions, test_iterations, max_missed)
-
-            self.gcode.respond_info(f"AUTO SPEED acceleleration measurement {step+1}\nMissed X {missed_x:.2f}, Y {missed_y:.2f} at a{accel}/v{veloc}")
-            
-            if not valid:
+            veloc = self._calc_velocity(accel, pos_travel)/math.log10(accel)
+            while attempt < test_attempts:
+                valid, duration, missed_x, missed_y = self._test(veloc, accel, positions, test_iterations, max_missed)
+                respond = f"AUTO SPEED acceleleration measurement {step+1} attempt {attempt}\n"
+                respond += f"Missed X {missed_x:.2f}, Y {missed_y:.2f} at a{accel:.0f}/v{veloc:.0f} over {duration:.2f}s"
+                self.gcode.respond_info(respond)
+                if valid:
+                    break
+                if missed_x > max_missed*10 or missed_y > max_missed*10:
+                    attempt = test_attempts
+                    break
+                attempt += 1
+            if attempt == test_attempts:
                 break
             measured_accel = step
-        self.gcode.respond_info(f"AUTO SPEED found maximum acceleration {accel_start + (accel_step * measured_accel)}, at velocity {veloc_start}")
+            measured_accel_veloc = veloc
+        accel_max = accel_start + (accel_step * measured_accel)
+        self.gcode.respond_info(f"AUTO SPEED found maximum acceleration {accel_max:.0f}, at velocity {measured_accel_veloc:.0f}")
 
         # Find velocity maximum
         measured_veloc = None
+        measured_veloc_accel = None
+        attempt = 1
         for step in range(0, veloc_count):
-            accel = accel_start + (accel_step * (measured_accel - 1))
             veloc = veloc_start + (veloc_step * step)
-            valid, missed_x, missed_y = self._test(veloc, accel, positions, test_iterations, max_missed)
-
-            self.gcode.respond_info(f"AUTO SPEED velocity measurement {step+1}\nMissed X {missed_x:.2f}, Y {missed_y:.2f} at v{veloc}/a{accel}")
-            
-            if not valid:
+            accel = self._calc_accel(veloc, pos_travel)*4
+            while attempt < test_attempts:
+                valid, duration, missed_x, missed_y = self._test(veloc, accel, positions, test_iterations, max_missed)
+                respond = f"AUTO SPEED velocity measurement {step+1} attempt {attempt}\n"
+                respond += f"Missed X {missed_x:.2f}, Y {missed_y:.2f} at v{veloc:.0f}/a{accel:.0f} over {duration:.2f}s"
+                self.gcode.respond_info(respond)
+                if valid:
+                    break
+                if missed_x > max_missed*10 or missed_y > max_missed*10:
+                    attempt = test_attempts
+                    break
+                attempt += 1
+            if attempt == test_attempts:
                 break
             measured_veloc = step
-        self.gcode.respond_info(f"AUTO SPEED found maximum velocity {veloc_start + (veloc_step * measured_veloc)}, at accel {accel_start + (accel_step * (measured_accel - 1))}")
+            measured_veloc_accel = accel
+        veloc_max = veloc_start + (veloc_step * measured_veloc)
+        self.gcode.respond_info(f"AUTO SPEED found maximum velocity {veloc_max:.0f}, at accel {measured_veloc_accel:.0f}")
 
         # Perform stress test
-        for step in range(100, 0, -1):
-            accel = accel_start + (accel_step * (measured_accel - (100 - step)))
-            veloc = veloc_start + (veloc_step * (measured_veloc - (100 - step)))
-
-            valid, missed_x, missed_y = self._test(veloc, accel, positions, stress_iterations, max_missed)
-
-            self.gcode.respond_info(f"AUTO SPEED stress measurement {(100 - step) + 1}\nMissed X {missed_x:.2f}, Y {missed_y:.2f} at a{accel}/v{veloc}")
-
+        for step in range(0, 100, 1):
+            accel = accel_start + (accel_step * measured_accel)
+            veloc = veloc_start + (veloc_step * measured_veloc)
+            valid, duration, missed_x, missed_y = self._test(veloc, accel, positions, stress_iterations, max_missed)
+            
+            respond = f"AUTO SPEED stress measurement {step + 1}\n"
+            respond += f"Missed X {missed_x:.2f}, Y {missed_y:.2f} at a{accel:.0f}/v{veloc:.0f} over {duration:.2f}s"
+            self.gcode.respond_info(respond)
+            
+            measured_accel -= step
+            measured_veloc -= step
+            
             if valid:
-                measured_accel -= (100 - step)
-                measured_veloc -= (100 - step)
                 break
 
+        accel_used = accel_start + (accel_step * (measured_accel))
+        veloc_used = veloc_start + (veloc_step * (measured_veloc))
+
         results = "AUTO SPEED Results:\n"
-        results += f"Recomended maximum acceleration: {accel_start + (accel_step * (measured_accel))}\n"
-        results += f"Recomended maximum velocity: {veloc_start + (veloc_step * measured_veloc)}"
+        results += f"Recomended maximum acceleration: {accel_used:.0f}\n"
+        results += f"Recomended maximum velocity: {veloc_used:.0f}\n"
         self.gcode.respond_info(results)
         return
     
@@ -250,11 +274,13 @@ class AutoSpeed:
         self.toolhead.wait_moves()
         self._home(True, True, False)
         start_steps = self._get_steps()
+        start = perf_counter()
         for _ in range(iterations):
             self._pattern_move(positions, veloc)
         #self._move([self.axes["x"]["center"], self.axes["y"]["center"], None], veloc)
-
         self.toolhead.wait_moves()
+        duration = perf_counter() - start
+
         self._home(True, True, False)
         stop_steps = self._get_steps()
 
@@ -273,7 +299,7 @@ class AutoSpeed:
             valid = False
         if missed_y > max_missed:
             valid = False
-        return valid, missed_x, missed_y
+        return valid, duration, missed_x, missed_y
 
     def _pattern_move(self, pos, speed):
         # Large pattern diagonals
@@ -335,6 +361,17 @@ class AutoSpeed:
         self.toolhead.max_accel = accel
         self.toolhead.requested_accel_to_decel = accel/2
         self.toolhead._calc_junction_deviation()
+
+    def _calc_velocity(self, accel: float, travel: float):
+        #self.gcode.respond_info(f"Calculating velocity using accel {accel:.2f} over {travel:.2f} distance")
+        return math.sqrt(travel/accel)*accel
+    
+    def _calc_accel(self, veloc: float, travel: float):
+        #self.gcode.respond_info(f"Calculating accel using velocity {veloc:.2f} over {travel:.2f} distance")
+        return veloc**2/travel
+    
+    def _calc_travel(self, x: float, y: float):
+        return math.sqrt(x**2 + y**2)
 
 
 def load_config(config):
